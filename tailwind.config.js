fd0fda6ec29c9ff2b27b2716df590408a8913d01
ckandy <chaozukandy@gmail.com>
2022-03-30 05:40:17 +0300

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	darkMode: true,
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				111: '1px',
+				1.8: '18px',
+				11: '11px',
+				13: '54px',
+				27: '27px',
+				152: '152px',
+				480: '480px',
+			},
+			height: {
+				27: '27px',
+			},
+			colors: {
+				cc: '#ccc',
+				f8: '#f8f8f8',
+			},
+		},
+		fontFamily: {
+			sans: ['Roboto', 'Arial'],
+		},
 	},
 	plugins: [],
 };
